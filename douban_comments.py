0d6250a1d36a667e350da08faa66004b9f752c11
kuz0 <33897621+kuz0@users.noreply.github.com>
2017-12-31 16:31:18 +0800

    Update douban_comments.py

--- douban_comments.py
+++ douban_comments.py
@@ -1,24 +1,16 @@
 import pandas
 import requests
 from lxml import etree
-# from bs4 import BeautifulSoup
 
-r = requests.get('https://book.douban.com/subject/1084336/comments/')
+url_format = 'https://book.douban.com/subject/1084336/comments/hot?p={}'
+urls = [url_format.format(str(i)) for i in range(1, 6, 1)]
 
-# comments = []
-# soup = BeautifulSoup(r.text, 'lxml')
-# items = soup.find_all('p', 'comment-content')
-# for i in items:
-#     comments.append(i.string)
-# df = pandas.DataFrame(comments)
-# df.to_csv('comments.csv')
+comments = []
+for url in urls:
+    r = requests.get(url)
+    s = etree.HTML(r.text)
+    items = s.xpath('//div[@class="comment"]/p/text()')
+    comments += items
 
-s = etree.HTML(r.text)
-items = s.xpath('//div[@class="comment"]/p/text()')
-
-with open('comments.txt', 'w', encoding='utf-8') as f:
-    for i in items:
-        f.write(i)
-
-df = pandas.DataFrame(items)
+df = pandas.DataFrame(comments)
 df.to_excel('comments.xlsx')
