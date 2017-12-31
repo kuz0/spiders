0f51bc77fc8f4e279753e1019bddee1ede5e6bf1
kuz0 <33897621+kuz0@users.noreply.github.com>
2017-12-31 10:57:24 +0800

    Update douban_comments.py

--- douban_comments.py
+++ douban_comments.py
@@ -1,16 +1,22 @@
 import pandas
 import requests
 from bs4 import BeautifulSoup
-
-r = requests.get('https://book.douban.com/subject/1084336/comments/')
-
-soup = BeautifulSoup(r.text, 'lxml')
-items = soup.find_all('p', 'comment-content')
-for i in items:
-    print(i.string, '\n')
+from lxml import etree
 
 comments = []
+r = requests.get('https://book.douban.com/subject/1084336/comments/')
+
+# soup = BeautifulSoup(r.text, 'lxml')
+# items = soup.find_all('p', 'comment-content')
+# for i in items:
+#     comments.append(i.string)
+#     print(i.string)
+
+s = etree.HTML(r.text)
+items = s.xpath('//div[@class="comment"]/p/text()')
 for i in items:
-    comments.append(i.string)
+    comments.append(i)
+    print(i)
+
 df = pandas.DataFrame(comments)
 df.to_csv('comments.csv')
